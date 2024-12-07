2168ebdc3d981230f1202ac3d1d297d5249d2fba
bruuno-bh <tutyebh2@gmail.com>
2024-12-07 13:11:00 +0100

    nav_adjust

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,9 +8,16 @@ const navItems = [
 ];
 
 export default function Navigation() {
-  const scrollToSection = (id: string) => {
+  const scrollToSection = (id) => {
     const element = document.getElementById(id);
-    element?.scrollIntoView({ behavior: 'smooth' });
+    const navbarHeight = document.querySelector('nav').offsetHeight; // Calcula a altura da barra de navegação
+    if (element) {
+      const elementPosition = element.offsetTop - navbarHeight; // Subtrai a altura do navbar
+      window.scrollTo({
+        top: elementPosition,
+        behavior: 'smooth',
+      });
+    }
   };
 
   return (
@@ -23,7 +30,7 @@ export default function Navigation() {
               onClick={() => scrollToSection(item.id)}
               className={`${item.color} ${
                 item.id === 'onde-assistir' ? 'text-black' : 'text-white'
-              } py-3 px-4 md:py-5 md:px-8 font-bold hover:opacity-90 transition-opacity rounded-lg text-sm md:text-lg flex-1 md:flex-initial w-full md:w-auto`}
+              } py-3 px-4 font-bold hover:opacity-90 transition-opacity rounded-lg text-sm md:text-base flex-1 md:flex-none md:px-6`}
             >
               {item.label}
             </button>
